1fda1ba0be351695b3139a8397fa4d65a4bc23db
Sean Madden <sdm350@gmail.com>
2014-06-05 11:13:11 -0400

    removed unused code

--- site/server.js
+++ site/server.js
@@ -4,13 +4,7 @@
 
 var express = require("express"),
 	config = require("./site_config.json"),
-	app = express(),
-	router = express.Router();
-
-//router.route("/")
-//	.get(function (req, res) {
-//	}
-//);
+	app = express();
 
 app.use("/", express.static(__dirname + "/layout"));
 app.use("/js", express.static(__dirname + "/js"));
@@ -18,5 +12,3 @@ app.use("/js", express.static(__dirname + "/js"));
 app.listen(config.port, function() {
 	console.log("Site listening on port", config.port);
 });
-
-//app.get("/", router);
