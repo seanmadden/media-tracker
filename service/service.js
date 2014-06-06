1668d4568fd3bae1e251ed33f2d2a24669f5c998
Sean Madden <sdm350@gmail.com>
2014-06-06 12:06:41 -0400

    Added sorting to movie titles

--- service/service.js
+++ service/service.js
@@ -24,14 +24,16 @@ router.use(function(req, res, next) {
 
 router.route("/movies")
 	.get(function(req, res) {
-		Movie.find(function(err, Movies) {
-			if (err) {
-				res.send(err);
-				return;
-			}
+		Movie.find()
+			.sort('title')
+			.exec(function(err, Movies) {
+				if (err) {
+					res.send(err);
+					return;
+				}
 
-			res.json(Movies);
-		});
+				res.json(Movies);
+			});
 	})
 	.post(function(req, res) {
 		var movie = new Movie();
