ac53a1f3e0983d1b94407f5644ffd08d0b7b9291
Azkar <sdm350@gmail.com>
2014-07-08 18:24:48 -0700

    added test to verify list creation works

--- service/test/serviceTest.js
+++ service/test/serviceTest.js
@@ -3,11 +3,25 @@ var request = require('supertest'),
     mocha = require('mocha');
 
 describe('GET lists', function() {
-    it('respond with json', function(done) {
+    it('responds with json', function(done) {
         request(app)
             .get('/api/lists')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+
+    it('responds with "List Created"', function(done) {
+        request(app)
+            .post('/api/lists')
+            .send({title: 'testList'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                if (res.body.message !== 'List Created') {
+                    return "List not created successfully";
+                }
+            })
             .expect(200, done);
     });
 });
