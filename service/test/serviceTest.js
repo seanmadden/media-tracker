c07b9227ac0f0767cc1e3861c7447b9d95616148
Azkar <sdm350@gmail.com>
2014-07-11 07:14:17 -0700

    refactored some tests added test to test adding a list item

--- service/test/serviceTest.js
+++ service/test/serviceTest.js
@@ -3,7 +3,7 @@ var request = require('supertest'),
     mocha = require('mocha');
 
 describe('GET lists', function() {
-    it('responds with json', function(done) {
+    it('responds with all lists', function(done) {
         request(app)
             .get('/api/lists')
             .set('Accept', 'application/json')
@@ -25,17 +25,26 @@ describe('GET lists', function() {
             .expect(200, done);
     });
 
-    it('responds with json', function(done) {
+    it('returns the contents of a list', function(done) {
         request(app)
             .get('/api/testList')
             .expect('Content-Type', /json/)
             .expect(200, done);
     });
 
-    it('responds with json (delete list)', function(done) {
-    request(app)
-        .delete('/api/testList')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
+    it('adds an item to our list', function(done) {
+        request(app)
+            .post('/api/testList')
+            .send({title: 'testItem'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+
+    it('deletes a list', function(done) {
+        request(app)
+            .delete('/api/testList')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
 });
