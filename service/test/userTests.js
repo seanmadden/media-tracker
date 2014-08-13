dc8fbe02d7bf6fb5ff86f2360274ab6e978b6394
Azkar <sdm350@gmail.com>
2014-08-12 21:01:54 -0700

    removed user tests. Need to rethink user management

--- service/test/userTests.js
+++ service/test/userTests.js
@@ -2,20 +2,22 @@
  * Created by Sean on 7/19/2014.
  */
 
-var request = require('supertest'),
-    app = require('./../service');
+//TODO: Let's think about this...
 
-describe('User tests', function() {
-
-    it('creates a new user', function(done) {
-        request(app)
-            .post('/api/user')
-            .send({email: 'testUser@example.com',
-                   password: 'temp'})
-            .expect(function(res) {
-                assert(res.body.message == 'User Created!');
-            })
-
-
-    })
-});
+//var request = require('supertest'),
+//    app = require('./../service');
+//
+//describe('User tests', function() {
+//
+//    it('creates a new user', function(done) {
+//        request(app)
+//            .post('/api/user')
+//            .send({email: 'testUser@example.com',
+//                   password: 'temp'})
+//            .expect(function(res) {
+//                assert(res.body.message == 'User Created!');
+//            })
+//
+//
+//    })
+//});
